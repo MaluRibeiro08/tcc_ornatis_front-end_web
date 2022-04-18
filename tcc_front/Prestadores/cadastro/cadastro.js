b0af99e03cc55dfcc109710e9e15f641ecc427fe
sarah1510 <79976817+Arielly24@users.noreply.github.com>
2022-04-18 11:21:23 -0300

    dando continuidade n consumo de api da tela de cadastro

--- tcc_front/Prestadores/cadastro/cadastro.js
+++ tcc_front/Prestadores/cadastro/cadastro.js
@@ -1,7 +1,6 @@
-const container_estabelecimento = document.getElementById("container_estabelecimento")
+const container_estabelecimento = document.getElementById("container_estabelecimento");
 const container_perfil_adm = document.getElementById("container_perfil_adm");
 const container_dados_localizacao = document.getElementById("container_dados_localizacao");
-const container_perfil_salao = document.getElementById("container_perfil_salao");
 const container_dados_pagamento = document.getElementById("container_dados_pagamento");
 const container_regras_negocio = document.getElementById("container_regras_negocio");
 const container_funcionamento = document.getElementById("container_funcionamento");
@@ -9,88 +8,92 @@ const container_login = document.getElementById("container_login");
 
 
 
-const proximo = document.getElementById("botao_proximo")
+const proximo = document.getElementById("botao_proximo");
 let numeroSessao = 1
 
 mudarSecao = () => 
 {
+    console.log(numeroSessao)
     if(numeroSessao == 1)
     {
         numeroSessao = 2
-        container_perfil_adm.style.display = "inline"
+        container_estabelecimento.style.display = "inline"
+        container_perfil_adm.style.display = "none"
         container_dados_localizacao.style.display = "none"
-        container_perfil_salao.style.display = "none"
-        container_dados_pagamento = "none"
-        container_regras_negocio = "none"
-        container_funcionamento = "none"
-        container_login = "none"
+        container_dados_pagamento.style.display = "none"
+        container_regras_negocio.style.display = "none"
+        container_funcionamento.style.display = "none"
+        container_login.style.display = "none"
+        console.log(numeroSessao)
         
     }
     else if(numeroSessao == 2) 
     {
         numeroSessao = 3
-        container_perfil_adm.style.display = "none"
-        container_dados_localizacao.style.display = "inline"
-        container_perfil_salao.style.display = "none"
-        container_dados_pagamento = "none"
-        container_regras_negocio = "none"
-        container_funcionamento = "none"
-        container_login = "none"
+        container_estabelecimento.style.display = "none"
+        container_perfil_adm.style.display = "inline"
+        container_dados_localizacao.style.display = "none"
+        container_dados_pagamento.style.display = "none"
+        container_regras_negocio.style.display = "none"
+        container_funcionamento.style.display = "none"
+        container_login.style.display = "none"
+        console.log(numeroSessao)
     } 
     else if (numeroSessao == 3)
     {
         numeroSessao = 4
+        container_estabelecimento.style.display =  "none"
         container_perfil_adm.style.display = "none"
-        container_dados_localizacao.style.display = "none"
-        container_perfil_salao.style.display = "inline"
-        container_dados_pagamento = "none"
-        container_regras_negocio = "none"
-        container_funcionamento = "none"
-        container_login = "none"
+        container_dados_localizacao.style.display = "inline"
+        container_dados_pagamento.style.display = "none"
+        container_regras_negocio.style.display = "none"
+        container_funcionamento.style.display = "none"
+        container_login.style.display = "none"
+        console.log(numeroSessao)
     }
     else if (numeroSessao == 4)
     {
         numeroSessao = 5
+        container_estabelecimento.style.display =  "none"
         container_perfil_adm.style.display = "none"
         container_dados_localizacao.style.display = "none"
-        container_perfil_salao.style.display = "none"
-        container_dados_pagamento = "inline"
-        container_regras_negocio = "none"
-        container_funcionamento = "none"
-        container_login = "none"
+        container_dados_pagamento.style.display = "inline"
+        container_regras_negocio.style.display = "none"
+        container_funcionamento.style.display = "none"
+        container_login.style.display = "none"
     }
     else if (numeroSessao == 5)
     {
         numeroSessao = 6
+        container_estabelecimento.style.display =  "none"
         container_perfil_adm.style.display = "none"
         container_dados_localizacao.style.display = "none"
-        container_perfil_salao.style.display = "none"
-        container_dados_pagamento = "none"
-        container_regras_negocio = "inline"
-        container_funcionamento = "none"
-        container_login = "none"
+        container_dados_pagamento.style.display = "none"
+        container_regras_negocio.style.display = "inline"
+        container_funcionamento.style.display = "none"
+        container_login.style.display = "none"
     }
     else if (numeroSessao == 6)
     {
         numeroSessao = 7
+        container_estabelecimento.style.display =  "none"
         container_perfil_adm.style.display = "none"
         container_dados_localizacao.style.display = "none"
-        container_perfil_salao.style.display = "none"
-        container_dados_pagamento = "none"
-        container_regras_negocio = "none"
-        container_funcionamento = "inline"
-        container_login = "none"
+        container_dados_pagamento.style.display = "none"
+        container_regras_negocio.style.display = "none"
+        container_funcionamento.style.display = "inline"
+        container_login.style.display = "none"
     }
     else if (numeroSessao == 7)
     {
-        numeroSessao = 1
+        numeroSessao = 1;
+        container_estabelecimento.style.display =  "none"
         container_perfil_adm.style.display = "none"
         container_dados_localizacao.style.display = "none"
-        container_perfil_salao.style.display = "none"
-        container_dados_pagamento = "none"
-        container_regras_negocio = "none"
-        container_funcionamento = "none"
-        container_login = "inline"
+        container_dados_pagamento.style.display = "none"
+        container_regras_negocio.style.display = "none"
+        container_funcionamento.style.display = "none"
+        container_login.style.display = "inline"
     }
 }
 
@@ -167,8 +170,9 @@ const salvarCadastro = () => {
         cnpj: document.getElementById('cnpj').value,
         contato: document.getElementById('contato').value,
         biografia: document.getElementById('biografia').value,
+        foto: document.getElementById('img_photo_estabelecimento'.src),
         //adm
-        foto: document.getElementById('fl_image').value,
+        foto: document.getElementById('fl_image').src,
         nome: document.getElementById('nomeAdministrador').value,
         data_de_nascimento: document.getElementById('data_nascimento').value,
         cpf: document.getElementById('cpf').value, 
