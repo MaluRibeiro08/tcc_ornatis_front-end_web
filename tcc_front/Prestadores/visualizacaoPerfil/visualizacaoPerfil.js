b02a2cd514b7853bb9df0bed94f4855e2bed2a97
Mayssa Martha <mayssasilva147@gmail.com>
2022-04-20 09:40:40 -0300

    perfil

---
+++ tcc_front/Prestadores/visualizacaoPerfil/visualizacaoPerfil.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const nome_perfil = document.getElementById('lblTitulo');
+const biografia_perfil = document.getElementById('lblBiografia');
+const endereco_perfil = document.getElementById('lblEndereco');
