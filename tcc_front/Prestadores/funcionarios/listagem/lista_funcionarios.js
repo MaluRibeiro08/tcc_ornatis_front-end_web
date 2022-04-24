4d3946649d6056fe7793f59eb5c471a18a084b36
Arielly24 <arielly.angelo2630@gmail.com>
2022-04-24 15:07:02 -0300

    correção das ações de editar

--- tcc_front/Prestadores/funcionarios/listagem/lista_funcionarios.js
+++ tcc_front/Prestadores/funcionarios/listagem/lista_funcionarios.js
@@ -1,3 +1,5 @@
+//abrir menu lateral
+
 function abrir_menu(){
     document.getElementById('menu').style.width = '500px';
 }
@@ -5,3 +7,42 @@ function abrir_menu(){
 function fechar_menu(){
     document.getElementById('menu').style.width = '0px'
 }
+
+//modal editar
+
+function abrirEditar(){
+    document.getElementById('popup_editar').style.display = "block"
+}
+
+function fecharEditar(){
+    document.getElementById('popup_editar').style.display = "none"
+}
+
+//modal excluir
+
+function abrirExcluir(){
+    document.getElementById('popup_excluir').style.display = "block"
+}
+
+function fecharExcluir(){
+    document.getElementById('popup_excluir').style.display = "none"
+}
+
+//fazer aparecer a imagem quando fizer upload (MODAL EDITAR)
+
+let photo_funcionario = document.getElementById('img_photo_funcionario');
+let file = document.getElementById('fl_image_funcionario');
+
+photo_funcionario.addEventListener('click', () => {
+    file.click();
+});
+
+file.addEventListener('change', (event) => {
+    let reader = new FileReader();
+
+    reader.onload = () => {
+        photo_funcionario.src = reader.result;
+    }
+
+    reader.readAsDataURL(file.files[0]);
+})
