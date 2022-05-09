ac894385511d7c48821c7964c47f25b945ed8753
sarah1510 <79976817+Arielly24@users.noreply.github.com>
2022-05-09 11:04:28 -0300

    consumo de api cadastro de serviço

--- tcc_front/servico/cadastro/cadastro_servico.js
+++ tcc_front/servico/cadastro/cadastro_servico.js
@@ -1,3 +1,5 @@
+//fazer aparecer a imagem quando fizer upload
+
 let photo_servico = document.getElementById('img_photo_servico');
 let file = document.getElementById('fl_image_servico');
 
@@ -5,7 +7,6 @@ photo_servico.addEventListener('click', () => {
     file.click();
 });
 
-//fazer aparecer a imagem quando fizer upload
 
   file.addEventListener('change', (event) => {
     let reader = new FileReader();
@@ -16,3 +17,82 @@ photo_servico.addEventListener('click', () => {
 
     reader.readAsDataURL(file.files[0]);
 })
+
+/**CONSUMO**/
+
+//const
+
+const input_titulo = document.getElementById('titulo_servico')
+const input_preco = document.getElementById('preco_servico')
+const input_promocao = document.getElementById('promocao_servico')
+const input_detalhes = document.getElementById('detalhes_servico')
+const input_tempo_producao = document.getElementById('tempo_producao_servico')
+const image_funcionario = document.getElementById('fl_image_servico')
+const radio_estabelecimento = document.getElementById('estabelecimento_servico')
+const radio_domicilio = document.getElementById('domicilio_servico')
+const select_publico_alvo = document.getElementById('publico_alvo')
+const select_categoria = document.getElementById('categoria')
+const select_parte_corpo = document.getElementById('parte_do_corpo')
+const select_funcionario_realizador = document.getElementById('funcionario_realizador') 
+const radio_servico_ativo = document.getElementById('servico_ativo')
+const radio_servico_desativo = document.getElementById('servico_desativo')
+const input_intervalo = document.getElementById('intervalo_servico')
+const btn_salvar_servico = document.getElementById('salvar_servico')
+
+//get de informacoes
+
+
+
+const cadastro_servico = (data) =>{
+
+
+    const options_servico = {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'content-type': 'application/json',
+        },
+    };
+    fetch(url, options_servico).then(response => response.json()).then(
+        data => 
+        {
+            const input_id_servico = document.getElementById("id_servico");
+
+            input_id_servico.value = data.data;
+        }
+    );
+};
+const get_dados_servico = () => 
+{
+    const data = {};
+    data ['acao'] = 'createServico';
+    data ['nome_servico'] = input_titulo.value;
+    data ['preco'] = input_preco.value;
+    data ['desconto'] = input_promocao.value;
+    data ['detalhes'] = input_detalhes.value;
+    data ['tempo_duracao'] = input_tempo_producao.value;
+    data ['imagem_servico'] = image_funcionario.value;
+    data ['id_tipo_atendimento'] = radio_estabelecimento.value;
+    data ['id_tipo_atendimento'] = radio_domicilio.value;
+    
+    return data
+};
+
+btn_salvar_servico.addEventListener("click", 
+    () =>
+    {
+        const dados = get_dados_funcionario();
+        cadastro_funcionario(dados);
+    }
+)
+
+
+
+
+
+
+
+
+
+
+
