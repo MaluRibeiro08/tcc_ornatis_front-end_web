a36935300e0921b24b59827258ab07277087bdb3
Maria Luiza Ribeiro Teixeira <maluribeiro08@gmail.com>
2022-05-28 17:38:37 -0300

    Unificando pastas relacionadas a funcionarios

--- tcc_front/Prestadores/visualizacaoPerfil/scripts/funcionarios.js
+++ tcc_front/Prestadores/visualizacaoPerfil/scripts/funcionarios.js
@@ -109,5 +109,15 @@ const salvarDadosFuncionario = (id_empresa, id_funcionario) =>
     );
 }
 
+const carregarFuncionarios =   (id_empresa) => fetch(`http://localhost/tcc_ornatis_back-end/api-ornatis/rotas/adm/funcionario/?id_empresa=${id_empresa}&acao=listarFuncionarios`)
 
-export {salvarDadosFuncionario}
+const listarFuncionarios = async (id_empresa) =>
+{
+    const response = await carregarFuncionarios(id_empresa);
+    const informacoes = await response.json();
+    
+    return informacoes;
+}
+
+
+export {salvarDadosFuncionario, listarFuncionarios}
