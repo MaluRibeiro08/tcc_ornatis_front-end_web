60395791140e2b8b51240462be5c4b9cbec4d270
Maria Luiza Ribeiro Teixeira <maluribeiro08@gmail.com>
2022-05-28 17:39:10 -0300

    Unificando arquivos relacionados a servico

--- tcc_front/Prestadores/visualizacaoPerfil/scripts/servicos/servicos.js
+++ tcc_front/Prestadores/visualizacaoPerfil/scripts/servicos/servicos.js
@@ -49,4 +49,33 @@ const setarDisponibilidadeServico = async (id_servico) =>
     
 }
 
-export {getServicosPorEmpresa, setarDisponibilidadeServico}
+
+const getEspecialidades = async (id_empresa) =>
+{
+
+    //buscando no banco
+    const response = await fetch(`http://localhost/tcc_ornatis_back-end/api-ornatis/rotas/adm/servico/?id_empresa=${id_empresa}&acao=listarEspecialidades`);
+    const informacoes = await response.json();
+    
+    return informacoes.data;
+}
+
+const getParteCorpoPorEspecialidade = async (id_empresa, id_especilaidade) =>
+{
+    //buscando no banco
+    const response = await fetch(`http://localhost/tcc_ornatis_back-end/api-ornatis/rotas/adm/servico/?id_empresa=${id_empresa}&id_especialidade=${id_especilaidade}&acao=listarEspecialidadePartesCorpo`);
+    const informacoes = await response.json();
+    
+    return informacoes.data;
+}
+
+const getPartesCorpo = async () => 
+{
+    //buscando no banco
+    const response = await fetch(`http://localhost/tcc_ornatis_back-end/api-ornatis/rotas/adm/servico/?acao=listarPartesCorpo`);
+    const informacoes = await response.json();
+    
+    return informacoes.data;
+}
+
+export {getServicosPorEmpresa, setarDisponibilidadeServico, getEspecialidades, getParteCorpoPorEspecialidade, getPartesCorpo}
