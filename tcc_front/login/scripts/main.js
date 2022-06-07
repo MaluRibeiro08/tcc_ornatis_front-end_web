ad9b2568b77b2191014fef2bae10382cf2fd8c3b
Maria Luiza Ribeiro Teixeira <maluribeiro08@gmail.com>
2022-06-07 08:19:16 -0300

    Implementação inicial do login

--- tcc_front/login/scripts/main.js
+++ tcc_front/login/scripts/main.js
@@ -5,34 +5,75 @@ const trocarTipoLogin = (tipo_selecionado) =>
 {
     if(tipo_selecionado == 'consumidor')
     {
+        //tirando o outro
+            document.getElementById("header_prestador").classList.remove("header_selecionado")
+            document.getElementById("header_prestador").classList.add("header_nao_selecionado")
+
+        //add o atual
+            document.getElementById("header_consumidor").classList.add("header_selecionado")
+            document.getElementById("header_consumidor").classList.remove("header_nao_selecionado")
+            document.getElementById("tipo_login_selecionado").value = "consumidor"
+            document.getElementById("link_cadastro").href = "../consumidores/cadastro/cadastro_consumidor.html"
+
         console.log('Vamos para consumidor');
     }
     else if (tipo_selecionado == 'prestador')
     {
-        console.log('vamos para prestador');
+        //tirando o outro
+            document.getElementById("header_consumidor").classList.remove("header_selecionado")
+            document.getElementById("header_consumidor").classList.add("header_nao_selecionado")
+       
+        //add o atual
+            document.getElementById("header_prestador").classList.add("header_selecionado")
+            document.getElementById("header_prestador").classList.remove("header_nao_selecionado")
+    
+            document.getElementById("tipo_login_selecionado").value = "prestador"
+            document.getElementById("link_cadastro").href = "../Prestadores/cadastro/cadastro_prestador.html"
+
+        console.log('Vamos para consumidor');
     }
 }
 
+const fazerLogin = () =>
+{
+    const tipo_login_desejado = document.getElementById("tipo_login_selecionado").value
 
-document.getElementById('item_prestador').addEventListener('click', ()=>
+    if(tipo_login_desejado == "consumidor")
     {
-        trocarTipoLogin('prestador')
+        window.location.href = "../consumidores/home/home.html"
     }
-)
-
-document.getElementById('item_consumidor').addEventListener('click', ()=>
+    if(tipo_login_desejado == "prestador")
     {
-        trocarTipoLogin('consumidor')
+        window.location.href = "../Prestadores/home/home.html"
     }
-)
+}
 
-// document.getElementById('header_prestador').addEventListener('click', ()=>
-//     {
-//         trocarTipoLogin('prestador')
-//     }
-// )
-// document.getElementById('item_consumidor').addEventListener('click', ()=>
-//     {
-//         trocarTipoLogin('consumidor')
-//     }
-// )
+//EVENTOS
+
+    //CLICK PARA ENTRAR
+        document.getElementById("btn_entrar").addEventListener("click", fazerLogin)
+
+
+    //TROCA TIPO LOGIN
+        document.getElementById('item_prestador').addEventListener('click', ()=>
+            {
+                trocarTipoLogin('prestador')
+            }
+        )
+
+        document.getElementById('item_consumidor').addEventListener('click', ()=>
+            {
+                trocarTipoLogin('consumidor')
+            }
+        )
+
+        // document.getElementById('header_prestador').addEventListener('click', ()=>
+        //     {
+        //         trocarTipoLogin('prestador')
+        //     }
+        // )
+        // document.getElementById('item_consumidor').addEventListener('click', ()=>
+        //     {
+        //         trocarTipoLogin('consumidor')
+        //     }
+        // )
